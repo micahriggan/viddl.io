aac221fd10bed38563094f21c0f509e254be5112
Micah Riggan <mriggan@bitpay.com>
2018-12-04 03:42:27 -0500

    Download page mostly works, but some slight issues with saving data from frontend, may need to pivot slightly

--- packages/youtube-playlist-downloader-be/src/index.ts
+++ packages/youtube-playlist-downloader-be/src/index.ts
@@ -1,22 +1,61 @@
 import express from "express";
-import * as youtubedl from "ytdl-core";
+import * as youtubedl from "youtube-dl";
 import cors from "cors";
 import IO = require("socket.io");
 import * as HTTP from "http";
+import "./polyfill";
 
 const app = express();
 app.use(cors());
 
-const cache = {};
+const cache: { [url: string]: Array<any> } = {};
 const queue = [];
+
+function fetchInfo(url, options = []) {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching video data for ${url}`);
+    youtubedl.getInfo(url, options, (err, data) => {
+      if (err) return reject(err);
+      else return resolve(data);
+    });
+  });
+}
+
+async function* iterateInfo(url, startIndex = 0, batchSize = 3) {
+  let failureCount = 0;
+  let lastBatch = [];
+  do {
+    try {
+      const batch = `${startIndex}-${startIndex + batchSize}`;
+      console.log("Fetching ", url, "video", batch);
+      lastBatch = (await fetchInfo(url, [
+        `--playlist-items=${batch}`
+      ])) as Array<any>;
+      // inclusive if successful
+      startIndex += batchSize + 1;
+      failureCount = 0;
+
+      for (let item of lastBatch) {
+        yield item;
+      }
+    } catch (e) {
+      if (batchSize <= 0) {
+        startIndex++;
+        batchSize = 3;
+        failureCount++;
+      } else {
+        batchSize--;
+      }
+    }
+  } while (startIndex == 0 || (lastBatch.length >= 0 && failureCount < 3));
+}
 
 app.use("/info/:url", async (req, res) => {
   const url = decodeURIComponent(req.params.url);
   if (cache[url]) {
     res.json(cache[url]);
   } else {
-    cache[url] = youtubedl.getInfo(url);
-    res.status(307).json({ status: `fetching data for ${url}` });
+    res.status(307).json({ status: `use websockets to fetch data for ${url}` });
   }
 });
 
@@ -29,13 +68,23 @@ app.use("/valid/:url", async (req, res) => {
 const port = 8080;
 const server = HTTP.createServer(app);
 const io = IO(server);
+
 io.on("connection", socket => {
   console.log("connection");
   socket.on("info", async url => {
-    if (!cache[url]) {
-      cache[url] = youtubedl.getInfo(url);
+    console.log('wsclient requested', url);
+    if (cache[url]) {
+      if (cache[url].length) {
+        cache[url].forEach(video => socket.emit(url, video));
+      }
+    } else {
+      cache[url] = [];
+      for await (let video of iterateInfo(url)) {
+        console.log(`video info fetched for ${url}`);
+        cache[url].push(video);
+        socket.emit(url, video);
+      }
     }
-    socket.emit(url, await cache[url]);
   });
 });
 
--- packages/youtube-playlist-downloader-fe/src/containers/download/download.tsx
+++ packages/youtube-playlist-downloader-fe/src/containers/download/download.tsx
@@ -1,20 +1,37 @@
+import "./download.css";
+
+import JsFileDownload from "js-file-download";
 import * as React from "react";
 import { RouteComponentProps } from "react-router";
 import * as request from "request-promise";
-import { Card, Input } from "semantic-ui-react";
+import { Button, Card, Icon, Image, Select } from "semantic-ui-react";
+import * as IO from "socket.io-client";
+const io = IO("ws://localhost:8080", {
+  reconnection: true,
+  transports: ["websocket"]
+});
 
 type IProps = RouteComponentProps<{ url: string }>;
 
 interface IVideo {
   id: string;
+  fulltitle: string;
   title: string;
   url: string;
   thumbnail: string;
   description: string;
   _filename: string;
   format_id: string;
+  formats: IFormat[];
 }
 
+interface IFormat {
+  format_id: string;
+  format_note: string;
+  format: string;
+  ext: string;
+  url: string;
+}
 interface IYTPlaylistItem {
   id: string;
   name: string;
@@ -23,9 +40,23 @@ interface IYTPlaylistItem {
 
 interface IState {
   videos: IVideo[];
+  selectedFormat: string;
 }
 
 export class DownloadContainer extends React.Component<IProps, IState> {
+  public state: IState = {
+    selectedFormat: "",
+    videos: []
+  };
+
+  constructor(props: IProps) {
+    super(props);
+    this.handleFileDownload = this.handleFileDownload.bind(this);
+    this.videoRow = this.videoRow.bind(this);
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.handleDownloadAll = this.handleDownloadAll.bind(this);
+  }
+
   public async getPlaylistItems(url: string) {
     const encodedUrl = encodeURIComponent(url);
     const resp = await request.get(
@@ -37,31 +68,123 @@ export class DownloadContainer extends React.Component<IProps, IState> {
 
   public async getVideoInfo(url: string) {
     const encodedUrl = encodeURIComponent(url);
-    const resp = await request.get(
-      `http://localhost:8080/info/${encodedUrl}`,
-      { json: true }
-    );
+    const resp = await request.get(`http://localhost:8080/info/${encodedUrl}`, {
+      json: true
+    });
     return resp as Promise<IVideo[]>;
   }
 
   public async componentDidMount() {
     const url = decodeURIComponent(this.props.match.params.url);
-    const videoInfo = await this.getVideoInfo(url);
-    window.console.log(videoInfo);
+    try {
+      const videoInfo = await this.getVideoInfo(url);
+      this.setState({ videos: videoInfo });
+      window.console.log(this.state.videos);
+    } catch (e) {
+      io.on("connect", () => {
+        window.console.log("Connected to websocket");
+        io.emit("info", url);
+        io.on(url, (info: IVideo) => {
+          this.setState({ videos: this.state.videos.concat([info]) });
+        });
+      });
+    }
+  }
+
+  public handleFormatChange(
+    event: React.FormEvent<HTMLSelectElement>,
+    { value }: { value: string }
+  ) {
+    window.console.log(value);
+    this.setState({ selectedFormat: value });
+  }
+
+  public handleDownloadAll() {
+    for (const video of this.state.videos) {
+      this.handleFileDownload(video)();
+    }
+  }
+
+  public downloadManySettings() {
+    const options = this.state.videos[0].formats.map(f => ({
+      key: f.format_id,
+      text: f.ext + " - " + f.format,
+      value: f.format
+    }));
+
+    return (
+      <div className="settings-container">
+        <Select
+          placeholder="Select a format"
+          options={options}
+          onChange={this.handleFormatChange}
+        />
+        <Button
+          basic={true}
+          color="blue"
+          content="Download All"
+          icon="download"
+          onClick={this.handleDownloadAll}
+          label={{
+            as: "a",
+            basic: true,
+            color: "blue",
+            content: this.state.videos.length,
+            pointing: "left"
+          }}
+        />
+      </div>
+    );
+  }
+
+  public handleFileDownload(video: IVideo) {
+    return () => {
+      const videoFormat = video.formats.find(
+        f => f.format === this.state.selectedFormat
+      );
+      if (videoFormat) {
+        const filename = video.fulltitle + '.' + videoFormat.ext;
+        window.console.log("Downloading", videoFormat.url, filename);
+        JsFileDownload(videoFormat.url, filename);
+      }
+    };
+  }
+
+  public videoRow(video: IVideo) {
+    return (
+      <div key={video._filename}>
+        <div className="container">
+          <Image src={video.thumbnail} />
+          <div className="overlay">
+            <Icon
+              name="download"
+              size="massive"
+              onClick={this.handleFileDownload(video)}
+            />
+          </div>
+        </div>
+        {video.fulltitle}
+      </div>
+    );
   }
 
   public render() {
     return (
-      <Card>
-        <Card.Content>
-          <Card.Header>viddl.io</Card.Header>
-          <Card.Meta>download videos</Card.Meta>
-          <Card.Description>
-            <Input action={{ icon: "search" }} placeholder="Search..." />
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra={true} />
-      </Card>
+      <div className="download-container">
+        <Card style={{ width: "100%" }}>
+          <Card.Content>
+            <Card.Header>viddl.io</Card.Header>
+            <Card.Meta>download videos</Card.Meta>
+            <Card.Description>
+              {this.state.videos.length > 0
+                ? this.downloadManySettings()
+                : null}
+              {this.state.videos.map(this.videoRow)}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra={true} />
+        </Card>
+      </div>
     );
   }
 }
