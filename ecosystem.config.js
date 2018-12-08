c63d42fc777afaf05a5d92de8bd6ebadbde30b29
Micah Riggan <mriggan@bitpay.com>
2018-12-08 12:12:10 -0500

    Adding pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'viddl-be',
+    script: './packages/viddl-be/ts_build/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      VIDDL_BACKEND_PORT: '80'
+    }
+  }],
+};
